e683f99d296461f31642fbd5ccd923aefb982f6e
Edward Timmer <edtimmer@gmail.com>
2019-01-09 16:12:30 -0500

    added user create test

--- src/index.js
+++ src/index.js
@@ -1,24 +1,5 @@
 import '@babel/polyfill';   //if get warning replace with '@babel/polyfill/noConflict'
-import { GraphQLServer, PubSub } from 'graphql-yoga';
-import db from './db';
-import { resolvers, fragmentReplacements } from './resolvers/index';
-import prisma from './prisma';
-
-const pubsub = new PubSub();
-
-const server = new GraphQLServer({
-  typeDefs: './src/schema.graphql',
-  resolvers,
-  context(request) {
-    return {
-      db,
-      pubsub,
-      prisma,
-      request
-    }
-  },
-  fragmentReplacements
-})
+import server from './server';
 
 //By default goes to localhost port 4000
 server.start({ port: process.env.PORT || 4000 }, () => {
---
+++ tests/user.test.js
@@ -0,0 +1,37 @@
+import 'cross-fetch/polyfill';
+import ApolloBoost, { gql } from 'apollo-boost';
+import prisma from '../src/prisma';
+import { extractFragmentReplacements } from 'prisma-binding';
+
+jest.setTimeout(1000000000);
+
+const client = new ApolloBoost({
+  uri: 'http://localhost:4000'
+});
+
+test('Should create a new user', async () => {
+  const createUser = gql`
+    mutation {
+      createUser (
+        data: {
+          name: "Andrew",
+          email: "andrew@example.com",
+          password: "MyPass123"
+        }
+      ) {
+        token,
+        user {
+          id
+        }
+      }
+    }
+  `
+
+  const response = await client.mutate({
+    mutation: createUser
+  });
+
+  const exists = await prisma.exists.User({ id: response.data.createUser.user.id });
+
+  expect(exists).toBe(true);
+});
